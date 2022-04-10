6f3be2e5278dd6a4a4dc10ff988093b4d7f52648
Claudia <pinguiri1987@gmail.com>
2022-04-10 16:15:53 -0500

    done

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use("/api/posts", require("./routes/posts"));
-app.use("/api/users", require("./routes/usersRouter"));
-app.use("/api/products", require("./routes/productsRouter"));
-app.use("/api/orders", require("./routes/ordersRouter"));
+app.use("/users", require("./routes/usersRouter"));
+app.use("/products", require("./routes/productsRouter"));
+app.use("/orders", require("./routes/ordersRouter"));
 
 app.listen(port, () => {
   console.log(`servidor corriendo en el puerto ${port}`);
